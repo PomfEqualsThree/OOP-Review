4df5f4ea72f5d7ab33f340eb1b8001fa56f9a508
DDelera <41572239+PomfEqualsThree@users.noreply.github.com>
2019-01-08 09:34:56 -0700

    Earned

--- src/CSharp.Language.Review/EarnedMark.cs
+++ src/CSharp.Language.Review/EarnedMark.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace CSharp.Language.Review
 {
-    class EarnedMark
+    public class EarnedMark : WeightedMark
     {
+        public int Possible { get; private set; }
+        private double _Earned;
+        public double Earned
+        {
+            get { return _Earned; }
+            set
+            {
+                if (value < 0 || value > Possible)
+                    throw new Exception("Invalid earned mark assigned");
+                _Earned = value;
+            }
+        }
+        public double Percent
+        { get { return (Earned / Possible) * 100; } }
+
+        public double WeightedPercent
+        { get { return Percent * Weight / 100; } }
+
+        public EarnedMark(WeightedMark markableItem, int possible, double earned)
+            : this(markableItem.Name, markableItem.Weight, possible, earned)
+        {
+        }
+        public EarnedMark(string name, int weight, int possible, double earned)
+            : base(name, weight)
+        //{
+        //    if (possible <= 0)
+        //        throw
+        //}
     }
 }
